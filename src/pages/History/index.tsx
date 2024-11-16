d0cb4c59d98402d845b5b39dc77f94fca7f10f01
Guilherme Pessoa <guipessoagith@gmail.com>
2024-11-16 13:32:04 -0300

    Updating (date-fns) Pomodoro functional activity history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,10 @@
 import { useContext } from "react";
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
 import { HistoryContainer, HistoryList, Status } from "./styles";
 import { CyclesContext } from "../../contexts/CyclesContext";
+
+
 
 export function History() {
 
@@ -27,7 +31,10 @@ export function History() {
                                 <tr key={cycle.id}>
                                     <td>{cycle.task}</td>
                                     <td>{cycle.minutesAmount} minutos</td>
-                                    <td>{cycle.startDate.toISOString()}</td>
+                                    <td>{formatDistanceToNow(cycle.startDate, {
+                                        addSuffix: true,
+                                        locale: ptBR,
+                                    })}</td>
                                     <td>
                                         { cycle.finishedDate && <Status statusColor="green">Concluído</Status> }
                                         { cycle.interruptedDate && <Status statusColor="red">Interrompido</Status> }
